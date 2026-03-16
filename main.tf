2057256a2cc4258c316ab5c4ce9ec29095855f69
Gaurav <gaurav.girase111@gmail.com>
2026-03-16 21:29:49 +0530

    Created new vpc and 3 private subnets

--- main.tf
+++ main.tf
@@ -101,3 +101,75 @@ resource "aws_iam_instance_profile" "vault_instance_profile" {
   name = "vault-ec2-profile"
   role = aws_iam_role.vault_ec2_role.name
 }
+
+# Networking 
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "vault-vpc"
+  }
+}
+
+# Private Subnet 1
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "private-subnet-1"
+  }
+}
+
+# Private Subnet 2
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "private-subnet-2"
+  }
+}
+
+# Private Subnet 3
+resource "aws_subnet" "private_3" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "ap-south-1c"
+
+  tags = {
+    Name = "private-subnet-3"
+  }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_3" {
+  subnet_id      = aws_subnet.private_3.id
+  route_table_id = aws_route_table.private_rt.id
+}
